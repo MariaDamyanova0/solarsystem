00a4df0ae61e39a4416e9db6a02a9c0de71702f6
Dimitar Stoev <dimitarg.stoev@gmail.com>
2024-03-27 21:49:54 +0200

    refactor: added restructure and some suggestions

--- script/navigation.js
+++ script/navigation.js
@@ -1,2 +1,20 @@
-const navElement = document.getElementById('navigation');
+const currentPath = window
+    .location
+    .pathname
+    .split('/')
+    .pop();
 
+const navLinks = document
+    .querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+    const href = link
+        .querySelector('a')
+        .getAttribute('href')
+        .split('/')
+        .pop();
+
+    if (href === currentPath) {
+        link.classList.add('active');
+    }
+});
